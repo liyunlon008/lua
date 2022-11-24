ec7016af4a9b1482ee2d5bc6202dde9bdc599818
liyunlon008 <50652028+liyunlon008@users.noreply.github.com>
2022-11-24 23:48:38 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -155,7 +155,7 @@ local function TDAH_fake_script() -- MainHub.Animate
 	end)
 	tweenFrameOFF.Completed:Connect(function()
 		if isfile(game.PlaceId..'_andromeda.txt') == false then (syn and syn.request or http_request)({ Url = "http://127.0.0.1:6463/rpc?v=1",Method = "POST",Headers = {["Content-Type"] = "application/json",["Origin"] = "https://discord.com"},Body = game:GetService("HttpService"):JSONEncode({cmd = "INVITE_BROWSER",args = {code = "gGHEDdTvH7"},nonce = game:GetService("HttpService"):GenerateGUID(false)}),writefile(game.PlaceId..'_andromeda.txt', "discord")})end
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/liyunlon008/lua/main/main.lua'))()
+        loadstring(game:HttpGet('https://cdn.jsdelivr.net/gh/liyunlon008/lua@main/main.lua'))()
 		game.CoreGui.MainHub:Destroy()
 	end)
 	
